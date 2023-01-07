2837975e9ff1043a66ef50cc3e6d91378323f142
Chen <richeagle2000@gmail.com>
2023-01-07 14:03:18 +0800

    Add Math Branch

--- GitBranchApp/src/GitBranchApp/MainApp.java
+++ GitBranchApp/src/GitBranchApp/MainApp.java
@@ -1,10 +1,22 @@
 package GitBranchApp;
 
+import java.math.MathContext;
+
 public class MainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("1st Version");
+		
+		Math cMath = new Math();
+		System.out.println();
+		System.out.println(cMath.add(10,5));
 	}
 
+}
+
+class Math{
+	int add(int a, int b) {
+		return a+b;
+	}
 }
